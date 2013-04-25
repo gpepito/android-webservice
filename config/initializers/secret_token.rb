9c50f2342a9e5669e1779e7748046360440e921c
gladys <gladys@li409-170.members.linode.com>
2013-04-25 04:09:10 -0400

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orangehrmapi::Application.config.secret_token = 'b688e6ced41f014a41262c321db4bc15c836a4d1de16b70d958f043c67af4a6861e67c6099f827e52384548c1a8fccbde77f77f48bc1483a732b451245a490b0'
